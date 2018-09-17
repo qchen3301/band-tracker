b3c68a25f17224f95fb699ff7d19433fbdfb023e
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-09-17 11:57:45 -0400

    finished seeds.js

--- db/schema.js
+++ db/schema.js
@@ -4,24 +4,24 @@ const Schema = mongoose.Schema //create variable to host mongoose's schema (I th
 
 //build schemas - remember to order in reverse of build because you cannot reference
 //before instansiating
-const GigSchema = new Schema {{
+const GigSchema = new Schema ({
     venue: String,
     city: String,
-    date: String
+    date: String,
     price: Number
-}}
+})
 
-const BandSchema = new Schema {{
+const BandSchema = new Schema ({
     bandName: String,
     genre: String,
     gigs: [GigSchema]
-}}
+})
 
-const UserSchema = new Schema {{
+const UserSchema = new Schema ({
     username: String,
     city: String,
     bands: [BandSchema]
-}}
+})
 
 //build variables to hold the models...which will reference the built schemas
 const GigModel = mongoose.model('Gig', GigSchema)
@@ -31,7 +31,7 @@ const UserModel = mongoose.model('User', UserSchema)
 //module.exports allows access to info in this file from external files. 
 //Module.exports only allows access to one THING...but that THING can be an object, a collection of smaller things
 module.exports = {
-    Gig = GigModel,   //key:value = Gig:GigModel
-    Band = BandModel,  //key:value = Band:BandModel
-    User = UserModel   //key:value = User:UserModel
+    Gig : GigModel,   //key:value = Gig:GigModel
+    Band : BandModel,  //key:value = Band:BandModel
+    User : UserModel   //key:value = User:UserModel
 }
---
+++ db/seeds.js
@@ -0,0 +1,53 @@
+//same as schema.js file, create var to hold mongoose, then initialize connection to mongodb with mongoose
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/band-tracer', {useNewUrlParser: true} )
+
+//create variable to hold schemas in schema.js file
+const Schema = require('./schema')
+console.log(Schema)
+
+//since Schema (the variable) holds require(./schema), which is an object with three keys (user, band and gig)
+//create three new variables to hold the specific keys of the object (Schema) we want
+//we want a user variable to hold Schema's user key, a band var to hold Schema's band key, and so on...
+// const User = Schema.User 
+// const Band = Schema.Band
+// const Gig = Schema.Gig
+
+//destructuring:
+// take the USER key from SCHEMA object and put it into a variable called User
+// const { User } = Schema
+// const { Band } = Schema
+// const { Gig } = Schema 
+
+//destructuring 2: declare multiple consts at the same time
+const { User, Band, Gig } = Schema
+
+
+//create new objects of schemas to populate database
+
+const monday = new Gig({
+    venue: 'PCM',
+    city: 'Atlanta',
+    date: 'Monday',
+    price: 3.50
+})
+
+const nickle = new Band ({
+    name: 'NickelBack',
+    genre: 'Awesome',
+    gigs: [monday]
+})
+
+const daniel = new User ({
+    username: 'Mancy',
+    city: 'Atlanta',
+    bands:[nickle]
+})
+
+daniel.save()   //.save() is a method that saves (duh) the information to the (mongodb) database. However, the database will IMMEDIATELY respond to this request with a promise, 
+                //not an actual resolution.
+    .then((data) => {   //.then() command = after the second act, the RESOLUTION, is returned, execute this function. Note: ((xxxyyyzzz) => {}) the parens around xxxyyyzzz are extraneous
+        console.log(data)
+        console.log('Done Seeding')
+        mongoose.connection.close()
+    })
